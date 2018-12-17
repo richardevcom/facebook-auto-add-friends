49edbaf5a3a41ae4ffaad7c479b2cda764d06a9b
Ričards Mucelāns <17192512+richardevcom@users.noreply.github.com>
2018-12-17 03:48:14 +0200

    Add fb-add-friends.js file

---
+++ fb-add-friends.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Fb Auto Invite
+// @namespace    https://www.richardev.com
+// @version      0.5.1
+// @description  Facebook Add Friends Script
+// @author       richardev
+// @match        https://www.facebook.com/find-friends/browser/
+// @grant        none
+// ==/UserScript==
+
+! function() {
+    "use strict";
+    var e = 1;
+    document.getElementById("fbSearchResultsBox").scrollIntoView(!0);
+    var l = setInterval(function() {
+            var e, l = document.querySelectorAll(".layerCancel");
+            for (e = 0; e < l.length; ++e) l[e].click()
+        }, 100),
+        t = setInterval(function() {
+            var n = document.querySelectorAll(".FriendRequestAdd");
+            e < n.length ? (n[e].scrollIntoView(!0), window.scrollBy(0, 96.5), n[e].click(), e++) : (clearInterval(t), clearInterval(l), window.location.reload(!1))
+        }, 1e3)
+}();
